a5fbcf5cbf1d668c1fc97e4107f5ddbb67c17e25
armellenguefack39@gmail.com <armellenguefack39@gmail.com>
2024-12-08 17:04:04 -0500

    Add dependencies and log test

---
+++ src/main/java/com/tk/BestTK/BestTkApplication.java
@@ -0,0 +1,14 @@
+package com.tk.BestTK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTkApplication.class, args);
+		System.out.println("Welcome to my Spring Boot App");
+	}
+
+}
---
+++ src/test/java/com/tk/BestTK/BestTkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.BestTK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
